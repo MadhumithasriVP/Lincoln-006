080f1d08801ac9ca4e40a55393c35b551aad2247
Madhumithasri V P <47061123+MadhumithasriVP@users.noreply.github.com>
2020-03-27 22:35:23 +0530

    Add files via upload

---
+++ MadhuPranesh/MadhuPranesh_Movie Ticket Booking/MovieTicketBooking/src/main/java/com/service/TicketService.java
@@ -0,0 +1,22 @@
+package com.service;
+
+import org.springframework.stereotype.Service;
+
+import com.model.Ticket;;
+@Service
+public class TicketService {
+
+
+public double calculateTotalCost(Ticket ticket)
+{
+double rs=0,s;
+if(ticket.getCircleType().equals("Queen"))
+s=250;
+else
+s=150;
+rs=s*ticket.getNoOfTickets();
+return rs;
+
+}
+
+}
