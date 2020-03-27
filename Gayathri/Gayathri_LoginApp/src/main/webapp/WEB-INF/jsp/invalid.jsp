c31b48a308bdab13983293804ead58ea27f952bc
Madhumithasri V P <47061123+MadhumithasriVP@users.noreply.github.com>
2020-03-27 21:32:11 +0530

    Add files via upload

---
+++ Gayathri/Gayathri_LoginApp/src/main/webapp/WEB-INF/jsp/invalid.jsp
@@ -0,0 +1,7 @@
+<html>
+<body  bgcolor="lavender">
+<h2>Sorry!! Invalid Credentials. Try again</h2>
+</body>
+</html>
+
+
---
+++ Gayathri/Gayathri_MovieTicketBooking/src/main/java/com/model/Ticket.java
@@ -0,0 +1,32 @@
+package com.model;
+import org.springframework.stereotype.Component;
+@Component
+
+public class Ticket {
+
+	private String movieName;
+	private int noOfTickets;
+	private String circleType;
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public int getNoOfTickets() {
+		return noOfTickets;
+	}
+	public void setNoOfTickets(int noOfTickets) {
+		this.noOfTickets = noOfTickets;
+	}
+	public String getCircleType() {
+		return circleType;
+	}
+	public void setCircleType(String circleType) {
+		this.circleType = circleType;
+	}
+	
+	
+	
+	
+}
